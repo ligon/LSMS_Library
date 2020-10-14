75371ba7cb9a9894d0933be58afaa03334397618
Ethan Ligon <ligon@berkeley.edu>
2020-10-14 16:51:30 -0700

    New tidy food_prices.py  for 2005-06.

---
+++ Uganda/2005-06/_/food_prices.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('../../_')
+from uganda import prices_and_units
+
+myvars = dict(fn='Uganda/2005-06/Data/GSEC14A.dta',item='h14aq2',HHID='hh', market='h14aq12',farmgate='h14aq13',units='h14aq3')
+
+prices_and_units(**myvars)
+
