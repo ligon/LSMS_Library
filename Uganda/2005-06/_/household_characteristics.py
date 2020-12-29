4b463b6eafa9bcebac0f42a976dbbe21769aa53b
Ethan Ligon <ligon@berkeley.edu>
2020-12-29 10:39:43 -0800

    Add code to extract household characteristics in Uganda.

---
+++ Uganda/2005-06/_/household_characteristics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('../../_/')
+import pandas as pd
+import numpy as np
+from uganda import age_sex_composition
+
+myvars = dict(fn='../Data/GSEC2.dta',
+              HHID='HHID',
+              sex='h2q4',
+              age='h2q9',
+              months_spent='h2q6')
+
+df = age_sex_composition(**myvars)
+
+df = df.filter(regex='ales ')
+
+df['log HSize'] = np.log(df.sum(axis=1))
+
+df.to_parquet('household_characteristics.parquet')
