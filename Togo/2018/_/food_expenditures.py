ab96a883412facb1970b88ceb5daa45c90e420c3
Ethan Ligon <ligon@berkeley.edu>
2021-02-28 10:52:02 -0800

    Change strategy for valuing consumption: use median unit values across pop.

--- Togo/2018/_/food_expenditures.py
+++ Togo/2018/_/food_expenditures.py
@@ -5,11 +5,48 @@ from togo import food_expenditures
 import numpy as np
 import pandas as pd
 import json
+from cfe.df_utils import broadcast_binary_op
 
 food = pd.read_stata('../Data/Togo_survey2018_fooditems_forEthan.dta')
-food = food.rename(columns={'hhid':'j','food_item':'i'})
 
-x=food[['j','i','item_value']]
+vars={'hhid': 'j',
+      's07bq01' : 'i',
+      's07bq02' : 'last week?', # Has your household consumed [XX] in the past 7 days. 
+      's07bq03a' : 'c',    # What is the total quantity of [XX] consumed in the past 7 days. Quantity. 
+      's07bq03b' : 'unit', # What is the total quantity of [XX] consumed in the past 7 days. Unit
+      's07bq03c' : 'unit modifier', # What is the total quantity of [XX] consumed in the past 7 days. Unit size. 
+      's07bq04' : 'home produced',  # What quantity of [XX] was home produced? (In the same unit)
+      's07bq05' : 'gift received', # What quantity of [XX] comes from a gift? (In the same unit)
+      's07bq06' : 'last purchase',  # When was the last time you bought [XX]?
+      's07bq07a' : 'purchased quantity', # What quantity of [XX] did you buy last time? Quantity
+      's07bq07b' : 'purchased unit', # What quantity of [XX] did you buy last time? Unit
+      's07bq07c' : 'purchased unit modifier', # What quantity of [XX] did you buy last time? Unit size
+      's07bq08': 'purchase value' # What was the value of [XX] bought the last time?
+      }
+
+food = food.rename(columns=vars).set_index(['j','i','unit','unit modifier'])
+
+# Get prices implied last purchase in previous 30 days
+
+purchases = food.reset_index().set_index(['j','i'])[['purchase value','purchased quantity','purchased unit','purchased unit modifier']]
+
+purchases['unit value'] = purchases['purchase value']/purchases['purchased quantity']
+
+unit_values = purchases.groupby(['i','purchased unit','purchased unit modifier']).median()['unit value'].dropna()
+
+c = food['c'].unstack(['i','unit','unit modifier'])
+
+idx = list(set(c.columns.tolist()).intersection(unit_values.index.tolist()))
+
+c = c[idx].stack(['i','unit','unit modifier'])
+
+p = unit_values[idx]
+p.index.names = ['i','unit','unit modifier']
+
+x = broadcast_binary_op(c,lambda x,y: x*y, p)
+
+x = x.groupby(['j','i']).sum().dropna().reset_index()
+
 x['j'] = x['j'].astype(int).astype(str)
 
 x = x.set_index(['j','i']).squeeze()
@@ -25,6 +62,6 @@ x = x.replace(0,np.nan)
 
 x = x.iloc[:,2:]
 
-x.to_parquet('food_expenditures2.parquet')
+x.to_parquet('food_expenditures.parquet')
 
 
