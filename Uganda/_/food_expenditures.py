46cce31b247e70ac71a2daee1ed064d3918c8218
Ethan Ligon <ligon@berkeley.edu>
2021-10-13 15:43:21 -0700

    Update to include 2018-19 data.

--- Uganda/_/food_expenditures.py
+++ Uganda/_/food_expenditures.py
@@ -6,14 +6,17 @@ Read food expenditures; use harmonized food labels.
 import pandas as pd
 
 x={}
-for t in ['2005-06','2009-10','2010-11','2011-12','2013-14','2015-16']:
+for t in ['2005-06','2009-10','2010-11','2011-12','2013-14','2015-16','2018-19']:
     x[t] = pd.read_parquet('../'+t+'/_/food_expenditures.parquet')
-    x[t] = x[t].stack('i')
+    x[t] = x[t].stack('i').dropna()
     x[t] = x[t].reset_index().set_index(['j','i']).squeeze()
 
 x = pd.DataFrame(x)
 x = x.stack().unstack('i')
 
 x.index.names=['j','t']
+x['m'] = 'Uganda'
+x = x.reset_index().set_index(['j','t','m'])
 
-x.to_parquet('food_expenditures.parquet')
+
+x.to_parquet('../var/food_expenditures.parquet')
