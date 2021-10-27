8a7dc416438564371bb247d3b6d91829c72d44ec
Bella Cheng <isabellacheng@berkeley.edu>
2021-10-27 15:49:24 -0700

    Updates to add 2019-20 Uganda data.

--- Uganda/_/food_expenditures.py
+++ Uganda/_/food_expenditures.py
@@ -1,3 +1,5 @@
+
+
 #!/usr/bin/env python
 """
 Read food expenditures; use harmonized food labels.
@@ -6,7 +8,7 @@ Read food expenditures; use harmonized food labels.
 import pandas as pd
 
 x={}
-for t in ['2005-06','2009-10','2010-11','2011-12','2013-14','2015-16','2018-19']:
+for t in ['2005-06','2009-10','2010-11','2011-12','2013-14','2015-16','2018-19','2019-20']:
     x[t] = pd.read_parquet('../'+t+'/_/food_expenditures.parquet')
     x[t] = x[t].stack('i').dropna()
     x[t] = x[t].reset_index().set_index(['j','i']).squeeze()
