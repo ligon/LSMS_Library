93545e2bae997c85bf39ddab5550ea2d319218a5
Ethan Ligon <ligon@berkeley.edu>
2022-03-17 10:37:26 -0700

    Sort out what "region" means in 2005 wave of Uganda data.

--- Uganda/2005-06/_/other_features.py
+++ Uganda/2005-06/_/other_features.py
@@ -13,4 +13,9 @@ myvars = dict(fn='Uganda/2005-06/Data/GSEC1.dta',
 
 df = other_features(**myvars)
 
+# Some "Central" households have region coded as 0?  These seem to be households in one of the 34
+# Enumeration Areas (comm) of Kampala.
+# See https://microdata.worldbank.org/index.php/catalog/1001/data-dictionary/F41?file_name=2005_GSEC1
+df = df.replace({'region':{'0':'Kampala'})
+
 df.to_parquet('other_features.parquet')
