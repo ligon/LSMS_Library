8e2edb384e711fcd28b83c8bab6c776810b9ccaf
Ethan Ligon <ligon@berkeley.edu>
2022-03-21 16:56:28 -0700

    Fix problem with variables specified for 2018-19.

--- Uganda/2018-19/_/food_expenditures.py
+++ Uganda/2018-19/_/food_expenditures.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import sys
 sys.path.append('../../_')
-from uganda import food_expenditures, change_id
+from uganda import food_expenditures
 
 myvars = dict(fn='Uganda/2018-19/Data/GSEC15B.dta',item='CEB01',HHID='hhid',
-              purchased='CEB07',
-              away='CEB09',
-              produced='CEB11',
-              given='CEB013')
+              purchased='CEB05',
+              away='CEB07',
+              produced='CEB09',
+              given='CEB011')
 
 x = food_expenditures(**myvars)
 
