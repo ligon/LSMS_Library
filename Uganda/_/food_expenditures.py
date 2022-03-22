2a5f0ecc31fb5fe0fe732b0abdc5a1eff169234e
Ethan Ligon <ligon@berkeley.edu>
2022-03-21 17:57:38 -0700

    Move Waves to uganda.py.

--- Uganda/_/food_expenditures.py
+++ Uganda/_/food_expenditures.py
@@ -3,7 +3,8 @@
 Read food expenditures; use harmonized food labels.
 """
 import pandas as pd
-from uganda import change_id
+import numpy as np
+from uganda import change_id, Waves
 
 def id_walk(df,wave,waves):
     
@@ -16,25 +17,16 @@ def id_walk(df,wave,waves):
             df = change_id(df)
 
     return df
-    
-# Data to link household ids across waves
-Waves = {'2005-06':(),
-         '2009-10':(),
-         '2010-11':(),
-         '2011-12':(),
-         '2013-14':('GSEC1.dta','HHID','HHID_old'),
-         '2015-16':('gsec1.dta','hh','HHID'),
-         '2018-19':('GSEC1.dta','hhid','t0_hhid'),
-         '2019-20':('HH/gsec1.dta','hhid','hhidold')}
 
-x={}
+x = {}
 
-for t in Waves.keys():
+for t in list(Waves.keys()):
     print(t)
     x[t] = pd.read_parquet('../'+t+'/_/food_expenditures.parquet')
     x[t] = id_walk(x[t],t,Waves)
     x[t] = x[t].stack('i').dropna()
     x[t] = x[t].reset_index().set_index(['j','i']).squeeze()
+    x[t] = x[t].replace(0,np.nan).dropna()
 
 df = pd.DataFrame(x)
 df.columns.name = 't'
