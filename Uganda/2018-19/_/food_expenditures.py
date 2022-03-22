332a89ca2fe1b6c14d1915cbbed9f5c04fb3cf8d
Ethan Ligon <ligon@berkeley.edu>
2022-03-21 23:09:10 -0700

    Change variables for 2018-19--note that these don't seem to match Q!

--- Uganda/2018-19/_/food_expenditures.py
+++ Uganda/2018-19/_/food_expenditures.py
@@ -3,11 +3,13 @@ import sys
 sys.path.append('../../_')
 from uganda import food_expenditures
 
+# See https://microdata.worldbank.org/index.php/catalog/3795/data-dictionary/F93?file_name=GSEC15B.dta
+# Note that notations on Q don't seem to match!
 myvars = dict(fn='Uganda/2018-19/Data/GSEC15B.dta',item='CEB01',HHID='hhid',
-              purchased='CEB05',
-              away='CEB07',
-              produced='CEB09',
-              given='CEB011')
+              purchased='CEB07', 
+              away='CEB09',
+              produced='CEB11',
+              given='CEB013')
 
 x = food_expenditures(**myvars)
 
--- Uganda/2018-19/_/food_quantities.py
+++ Uganda/2018-19/_/food_quantities.py
@@ -4,11 +4,14 @@ import sys
 sys.path.append('../../_')
 from uganda import food_quantities
 
+# See https://microdata.worldbank.org/index.php/catalog/3795/data-dictionary/F93?file_name=GSEC15B.dta
+# Note that notations on Q don't seem to match!
+
 myvars = dict(fn='Uganda/2018-19/Data/GSEC15B.dta',item='CEB01',HHID='hhid',
-              purchased='CEB04',
-              away='CEB06',
-              produced='CEB08',
-              given='CEB10',
+              purchased='CEB06',
+              away='CEB08',
+              produced='CEB10',
+              given='CEB012',
               units='CEB03C')
 
 q = food_quantities(**myvars)
