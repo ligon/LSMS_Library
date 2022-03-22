4dcb0e8369a1d55ebdb1197b79271f6a7e45b421
Ethan Ligon <ligon@berkeley.edu>
2022-03-21 18:39:02 -0700

    Create food_quantities.py for 2019-20.

---
+++ Uganda/2019-20/_/food_quantities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('../../_')
+from uganda import food_quantities
+
+myvars = dict(fn='Uganda/2019-20/Data/HH/gsec15b.dta',
+              item='CEB01',
+              HHID='hhid',
+              purchased='CEB06',
+              away='CEB08',
+              produced='CEB10',
+              given='CEB12',
+              units='CEB03C')
+
+q = food_quantities(**myvars)
+
+q.to_parquet('food_quantities.parquet')
+
