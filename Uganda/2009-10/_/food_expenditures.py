168249d034eebf4cabfedd326a7725026bdd6a87
Ethan Ligon <ligon@berkeley.edu>
2022-07-13 17:59:56 -0700

    Modify food_expenditures.py to drop 2009 observation with bogus item code.

--- Uganda/2009-10/_/food_expenditures.py
+++ Uganda/2009-10/_/food_expenditures.py
@@ -11,5 +11,12 @@ myvars = dict(fn='Uganda/2009-10/Data/GSEC15b.dta',item='h15bq2',HHID='hh',
 
 x = food_expenditures(**myvars)
 
+# File includes one observation with bogus item code 170; drop!
+
+x=x.stack('i')
+new = x.reset_index('i')
+new = new.loc[new['i']!='Pineapple']
+x = new.reset_index().set_index(['j','i']).unstack('i')
+
 x.to_parquet('food_expenditures.parquet')
 
