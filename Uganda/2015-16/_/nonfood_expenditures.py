e482e99ef283d60aeea2b054c02cfdc2f0edf95c
Ethan Ligon <ligon@berkeley.edu>
2022-10-29 05:11:56 -0700

    Fix hhids for nonfood expenditures in 2015-16.

--- Uganda/2015-16/_/nonfood_expenditures.py
+++ Uganda/2015-16/_/nonfood_expenditures.py
@@ -2,6 +2,8 @@
 import sys
 sys.path.append('../../_')
 from uganda import nonfood_expenditures
+import dvc.api
+from lsms import from_dta
 
 myvars = dict(fn='../Data/gsec15c.dta',
               item='itmcd',
@@ -11,5 +13,14 @@ myvars = dict(fn='../Data/gsec15c.dta',
               produced='h15cq7',
               given='h15cq9')
 
-x = nonfood_expenditures(**myvars) 
+x = nonfood_expenditures(**myvars)
+
+# "Wrong" hhid variable; get correct one from gsec1
+with dvc.api.open('../Data/gsec1.dta',mode='rb') as f:
+    ids = from_dta(f)[['HHID','hh']]
+
+ids = ids.set_index('hh').squeeze().to_dict()
+
+x = x.replace({'hhid':ids})
+
 x.to_parquet('nonfood_expenditures.parquet')
