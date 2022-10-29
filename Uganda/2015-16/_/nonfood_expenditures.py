03e494ab5b7054c335fcb1510f810fe8002ae823
Amy Ai <amyai1@Amy-Ai-2.local>
2022-10-29 04:30:40 -0700

    Fix hhid in nonfood expenditure for Uganda 2015-16

--- Uganda/2015-16/_/nonfood_expenditures.py
+++ Uganda/2015-16/_/nonfood_expenditures.py
@@ -2,6 +2,9 @@
 import sys
 sys.path.append('../../_')
 from uganda import nonfood_expenditures
+import dvc.api
+from lsms import from_dta
+import pandas as pd 
 
 myvars = dict(fn='../Data/gsec15c.dta',
               item='itmcd',
@@ -12,4 +15,15 @@ myvars = dict(fn='../Data/gsec15c.dta',
               given='h15cq9')
 
 x = nonfood_expenditures(**myvars) 
+
+#general hh dataset 
+with dvc.api.open('../Data/gsec1.dta',mode='rb') as dta: 
+    a = from_dta(dta)
+a = a[["hh", "HHID"]].rename(columns={'hh': 'j', 'HHID': 'correct_hhid'})
+#replace wrong hhid  
+x = x.reset_index()
+x = pd.merge(x, a, on='j')
+x = x.drop('j', axis=1).rename(columns={'correct_hhid':'j'}).set_index('j')
+x.columns.name = 'i'
+
 x.to_parquet('nonfood_expenditures.parquet')
