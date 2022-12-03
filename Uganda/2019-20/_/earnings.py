752721b6998470df5a51a687d3d8f3c8175b1be0
Ethan Ligon <ligon@berkeley.edu>
2022-12-02 19:36:02 -0500

    Add code to extract labor earnings.

---
+++ Uganda/2019-20/_/earnings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import dvc.api
+from lsms import from_dta
+
+fn = '../Data/HH/gsec8.dta'
+earnings1 = 's8q78'  # Earnings from first job
+earnings2 = 's8q80'  # Earnings from second job
+
+with dvc.api.open(fn,mode='rb') as dta:
+    df = from_dta(dta)
+
+earnings = df.groupby('hhid')[[earnings1,earnings2]].sum().sum(axis=1)
+
+earnings.index.name = 'j'
+
+pd.DataFrame({"Earnings":earnings}).to_parquet('earnings.parquet')
---
+++ Uganda/_/earnings.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Compile data on reported labor earnings.
+"""
+import pandas as pd
+import numpy as np
+from uganda import change_id, Waves
+
+def id_walk(df,wave,waves):
+    
+    use_waves = list(waves.keys())
+    T = use_waves.index(wave)
+    for t in use_waves[T::-1]:
+        if len(waves[t]):
+            df = change_id(df,'../%s/Data/%s' % (t,waves[t][0]),*waves[t][1:])
+        else:
+            df = change_id(df)
+
+    return df
+
+x = {}
+
+for t in list(Waves.keys()):
+    print(t)
+    x[t] = pd.read_parquet('../'+t+'/_/earnings.parquet')
+    x[t] = id_walk(x[t],t,Waves).squeeze()
+
+x = pd.DataFrame(x)
+x.columns.names = ['t']
+x = pd.DataFrame({'earnings':x.stack()})
+
+x['m'] = 'Uganda'
+x = x.reset_index().set_index(['j','t','m'])
+
+x.to_parquet('../var/earnings.parquet')
