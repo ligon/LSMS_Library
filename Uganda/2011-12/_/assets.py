1d9099e2a7fdb3a106347cd9c7654d43d83d1a31
Ethan Ligon <ligon@berkeley.edu>
2022-12-04 09:25:29 -0500

    Add code to for value of assets.

---
+++ Uganda/2011-12/_/assets.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import pandas as pd
+import dvc.api
+from lsms import from_dta
+
+fn = '../Data/GSEC14.dta'
+
+with dvc.api.open(fn,mode='rb') as dta:
+    df = from_dta(dta)
+
+assets = df.groupby('HHID')['h14q5'].sum().replace(0,np.nan)
+
+assets.index.name = 'j'
+assets.name = 'assets'
+
+pd.DataFrame({"Assets":assets}).to_parquet('assets.parquet')
