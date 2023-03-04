a5092688bc24206ae1f5992d323665d9b80bd69a
Ethan Ligon <ligon@berkeley.edu>
2023-03-03 18:06:36 -0800

    Add household_characteristics and food_acquired to Ethiopia 2018-19.

---
+++ Ethiopia/2018-19/_/food_acquired.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+sys.path.append('../../_/')
+from ethiopia import food_acquired
+
+fn='../Data/sect6a_hh_w4.dta'
+
+myvars = dict(item='item_cd',                  # Code label uniquely identifying food
+              HHID='household_id',                # Unique household id
+              quantity = 's6aq02a',           # Quantity of food consumed
+              units = 's6aq02b',              # Units for food consumed
+              value_purchased  = 's6aq04',     # Total value of food purchased
+              quantity_purchased = 's6aq03a', # Quantity of food purchased
+              units_purchased = 's6aq03b')
+
+df = food_acquired(fn,myvars)
+
+df.to_parquet('food_acquired.parquet')
---
+++ Ethiopia/2018-19/_/household_characteristics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append('../../../_/')
+import pandas as pd
+import numpy as np
+from local_tools import age_sex_composition
+
+myvars = dict(fn='../Data/sect1_hh_w4.dta',
+              HHID='household_id',
+              sex='s1q02',
+              age='s1q03a')
+
+df = age_sex_composition(**myvars)
+
+mydf = df.copy()
+
+df = df.filter(regex='ales ')
+
+df['log HSize'] = np.log(df.sum(axis=1))
+
+df.to_parquet('household_characteristics.parquet')
