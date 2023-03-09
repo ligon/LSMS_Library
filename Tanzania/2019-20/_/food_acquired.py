23522b87d150d05d083af841f94607974f9afcb7
Amy Ai <amyai227@berkeley.edu>
2023-03-08 17:44:12 -0800

    Clean code and add functions in tanzania.py

--- Tanzania/2019-20/_/food_acquired.py
+++ Tanzania/2019-20/_/food_acquired.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 sys.path.append('../../_/')
-from tanzania import food_acquired
+from tanzania import food_acquired, new_harmonize_units
 import numpy as np
 
 fn='../Data/HH_SEC_J1.dta'
@@ -25,29 +25,11 @@ d = food_acquired(fn,myvars)
 d['t'] = '2019-20'
 df = d.reset_index().set_index(['j','t','i'])
 
-pair = {'quant': ['quant_ttl_consume', 'quant_purchase', 'quant_own', 'quant_inkind'] ,
-        'unit': ['unit_ttl_consume', 'unit_purchase', 'unit_own', 'unit_inkind']}
-
 unit_conversion = {'Kg': 1,
                    'Gram': 0.001,
                    'Litre': 1,
                    'Millilitre': 0.001,
                    'Piece': 'p'}
-
-df = df.fillna(0).replace(unit_conversion).replace('NONE', 0)
-pattern = r"[p+]"
-for i in range(4):
-    df[pair['quant'][i]] = df[pair['quant'][i]].astype(np.int64) * df[pair['unit'][i]]
-    df[pair['quant'][i]].replace('', 0, inplace=True)
-    if df[pair['quant'][i]].dtype != 'O':
-        df[pair['unit'][i]] = 'kg'
-    else: 
-        df[pair['unit'][i]] = np.where(df[pair['quant'][i]].str.contains(pattern).to_frame() == True, 'piece', 'kg')
-        df[pair['quant'][i]] = df[pair['quant'][i]].apply(lambda x: x if str(x).count('p') == 0 else str(x).count('p'))
-
-df['agg_u'] = df[pair['unit']].apply(lambda x: max(x) if min(x) == max(x) else min(x) + '+' + max(x), axis = 1)
-
-df['unitvalue_purchase'] = df['value_purchase']/df['quant_purchase']
-df.replace([np.inf, -np.inf, 0], np.nan, inplace=True)
+df = new_harmonize_units(df, unit_conversion)
 
 df.to_parquet('food_acquired.parquet')
