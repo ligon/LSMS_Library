6e990d0fc5512eb777c46dee328b2c4064b1b2b7
Ethan Ligon <ligon@berkeley.edu>
2023-09-04 13:31:55 -0600

    Add "m" as index to food_expenditures.

--- Senegal/_/food_prices_quantities_and_expenditures.py
+++ Senegal/_/food_prices_quantities_and_expenditures.py
@@ -5,20 +5,16 @@ import pandas as pd
 df = pd.read_parquet('../var/food_acquired.parquet')
 df.index = df.index.rename({'units':'u'})
 
-x = df[['last expenditure']]
-x = x.groupby(['j','t','i']).sum()
+x = df[['last expenditure']].groupby(['j','t','m','i']).sum()
 x.to_parquet('../var/food_expenditures.parquet')
-x.unstack('i').to_csv('~/Downloads/food_expenditures.csv')
 
 p = df['price'].groupby(['t','m','i','u']).median()
 p = p.reset_index()
 p['t'] = p['t'].astype(str)
 p = p.set_index(['t','m','i','u'])
 p.unstack('t').to_parquet('../var/food_prices.parquet')
-p.unstack('t').to_csv('~/Downloads/food_prices.csv')
 
 q = df['quantity']
 q = q.dropna()
 
 pd.DataFrame({'Quantity':q}).to_parquet('../var/food_quantities.parquet')
-q.squeeze().unstack('i').to_csv('~/Downloads/food_quantities.csv')
