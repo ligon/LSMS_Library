6d9f2a9e50fc37d71d4a36edc4016e767050d6b7
Ethan Ligon <ligon@berkeley.edu>
2024-03-14 16:02:23 -0700

    More on effort to build a python package.

---
+++ _/country.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import pandas as pd
+from importlib.resources import files
+
+
+class Country:
+    def __init__(self,country_name):
+        self.name = country_name
+
+    @property
+    def resources(self):
+        var = files("lsms_library") / "countries" / self.name / "var"
+
+        return var
+
+    def read_parquet(self,parquet):
+        try:
+            x = pd.read_parquet((self.resources / f'{parquet}.parquet'))
+        except FileNotFoundError:
+            print("Need to build {parquet}")
+
+        return x
+
+    def food_expenditures(self):
+        return self.read_parquet('food_expenditures')
+
+    def household_characteristics(self):
+        return self.read_parquet('household_characteristics')
