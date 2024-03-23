a2a5f0104aea35accd7ac06c6f86e4934786abee
Ethan Ligon <ligon@berkeley.edu>
2024-03-22 20:40:01 -0700

    Modified to use new df_data_grabber.

--- Tanzania/2020-21/_/other_features.py
+++ Tanzania/2020-21/_/other_features.py
@@ -1,39 +1,24 @@
 #!/usr/bin/env python
-
-import sys
-sys.path.append('../../_/')
-import pandas as pd
 import numpy as np
-import dvc.api
-from lsms import from_dta
 import sys
-sys.path.append('../../_/')
-from tanzania import other_features
+sys.path.append('../../../_/')
+from local_tools import df_data_grabber, to_parquet
 
-round_match = {1:'2008-09', 2:'2010-11', 3:'2012-13', 4:'2014-15'}
+idxvars = dict(j='y5_hhid',
+               m='domain')
 
-myvars = dict(fn='../Data/hh_sec_a.dta',
-              HHID='y5_hhid',
-              urban='y5_rural',
-              region='domain',
-              urban_converter = lambda x: True if x.lower()=='urban' else False)
+myvars = dict(Rural=('y5_rural',lambda x: x.lower()!='urban'))
 
-df = other_features(**myvars)
+df = df_data_grabber('../Data/hh_sec_a.dta',idxvars,**myvars)
 
-df['Rural'] = 1 - df.urban.astype(int)
+# There are hundreds of nans in domain, but looking at "region1",
+# which is a finer geographic division, suggests that these are all
+# in the "mainland other urban" domain.
+df = df.rename({np.nan:'Mainland Other Urban'},level='m')
 
-df['wave'] = '2020-21'
-
-df = df.rename(columns={'region':'m','wave':'t'})
-
-df['m'] = df['m'].replace({'Nan': np.nan, 'NAN': np.nan, 'nan': np.nan, 'NaN': np.nan}).fillna('Tanzania')
-
-df = df.reset_index().set_index(['j','t','m'])
-df = df[['Rural']]
-
-regions = set(df.index.get_level_values('m'))
-df = df.rename(index={k:k.title() for k in regions})
+# Capitalization of regions is irregular...
+df = df.rename(index=lambda s:s.title(),level='m')
 
 assert df.index.is_unique, "Non-unique index!  Fix me!"
 
-df.to_parquet('other_features.parquet')
+to_parquet(df,'other_features.parquet')
