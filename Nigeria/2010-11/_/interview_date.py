3bf49ff9f5ec08add75bccf4c08b81b5763d7497
cefayefang <ce_fang@berkeley.edu>
2024-07-12 16:45:16 -0700

    update Nigeria dta format data files and edit corresponding interview_date.py files

--- Nigeria/2010-11/_/interview_date.py
+++ Nigeria/2010-11/_/interview_date.py
@@ -1,55 +1,29 @@
 import numpy as np
 import pandas as pd
 import sys
-sys.path.append('../../../_/')
+sys.path.append('../../../_')
 from local_tools import df_data_grabber, to_parquet
-import dvc.api
-from lsms import from_dta
 
-##Harvest 2011
-fn='../Data/secta_harvestw1.csv'
-with dvc.api.open(fn,mode='rb') as csv:
-    df =  pd.read_csv(csv)
-vars = {'hhid': 'j', 'saq13y': 'year', 'saq13m': 'month', 'saq13d': 'day'}
-t = '2011'
-df=df[['hhid','saq13y', 'saq13m', 'saq13d']]
-df['t']=t
-df=df.rename(columns=vars)
-df=df.set_index(['j','t'])
+
+#Harvest '2010Q3'
+idxvars = dict(j='hhid',
+                t=('sector', lambda x: "2010Q3"))
+myvars = dict(year='saq13y',
+                month='saq13m',
+                day='saq13d')
+df = df_data_grabber('../Data/Post Harvest Wave 1/Household/secta_harvestw1.dta',idxvars,**myvars)
 df['date'] = pd.to_datetime(df[['year', 'month', 'day']], errors='coerce')
 df=df.drop(columns=['year','month','day'])
 
-
-##Planting(2011)
-fn='../Data/secta_plantingw1.csv'
-with dvc.api.open(fn,mode='rb') as csv:
-    df_1 =  pd.read_csv(csv)
-t = '2011'
-df_1=df_1[['hhid','saq13y', 'saq13m', 'saq13d']]
-df_1['t']=t
-df_1=df_1.rename(columns=vars)
-df_1=df_1.set_index(['j','t'])
+#Planting(2011Q1)
+idxvars = dict(j='hhid',
+                t=('sector', lambda x: "2011Q1"))
+myvars = dict(year='saq13y',
+                month='saq13m',
+                day='saq13d')
+df_1 = df_data_grabber('../Data/Post Planting Wave 1/Household/secta_plantingw1.dta',idxvars,**myvars)
 df_1['date'] = pd.to_datetime(df_1[['year', 'month', 'day']], errors='coerce')
 df_1=df_1.drop(columns=['year','month','day'])
 
 df = pd.concat([df,df_1])
 to_parquet(df,'interview_date.parquet')
-
-##Harvest '2013Q2'
-# idxvars = dict(j='hhid',
-#                 t=('sector', lambda x: "2013Q2"))
-# myvars = dict(year='saq13y',
-#                 month='saq13m',
-#                 day='saq13d')
-# df = df_data_grabber('../Data/secta_harvestw1.csv',idxvars,**myvars)
-# df['date'] = pd.to_datetime(df[['year', 'month', 'day']], errors='coerce')
-# df=df.drop(columns=['year','month','day'])
-##Planting(2010Q3)
-# idxvars = dict(j='hhid',
-#                 t=('sector', lambda x: "2010Q3"))
-# myvars = dict(year='saq13y',
-#                 month='saq13m',
-#                 day='saq13d')
-# df_1 = df_data_grabber('../Data/secta_plantingw1.csv',idxvars,**myvars)
-# df_1['date'] = pd.to_datetime(df_1[['year', 'month', 'day']], errors='coerce')
-# df_1=df_1.drop(columns=['year','month','day'])
--- Nigeria/2018-19/_/interview_date.py
+++ Nigeria/2018-19/_/interview_date.py
@@ -4,12 +4,26 @@ import sys
 sys.path.append('../../../_/')
 from local_tools import df_data_grabber, to_parquet
 
+#Planting(2019Q1)
 idxvars = dict(j='hhid',
-               t=('hhid', lambda x: "2018-19"),
+               t=('hhid', lambda x: "2019Q1"),
                )
 
-myvars = dict(date='InterviewStart')
+myvars = dict(date=('InterviewStart', lambda x: pd.to_datetime(x).date))
 
-df = df_data_grabber('../Data/secta_plantingw4.dta',idxvars,**myvars)
+df_planting = df_data_grabber('../Data/secta_plantingw4.dta',idxvars,**myvars)
 
+
+
+#Harvest(2018Q3)
+idxvars = dict(j='hhid',
+               t=('hhid', lambda x: "2018Q3"),
+               )
+
+myvars = dict(date=('InterviewStart', lambda x: pd.to_datetime(x).date))
+
+df = df_data_grabber('../Data/secta_harvestw4.dta',idxvars,**myvars)
+
+
+df=pd.concat([df_planting,df],axis=0)
 to_parquet(df,'interview_date.parquet')
