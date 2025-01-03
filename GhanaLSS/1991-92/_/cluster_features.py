170017a9a06736592826b90f73f6427744d64de5
Ethan Ligon <ligon@berkeley.edu>
2025-01-03 07:42:50 -0800

    Modify to use w (wave) index.

--- GhanaLSS/1991-92/_/cluster_features.py
+++ GhanaLSS/1991-92/_/cluster_features.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python
 import sys
-sys.path.append('../../_/')
-import pandas as pd
-import numpy as np
-from lsms import from_dta
 sys.path.append('../../../_/')
-from local_tools import df_data_grabber, to_parquet, df_from_orgfile, format_id
+from local_tools import df_data_grabber, to_parquet, df_from_orgfile, format_id, get_categorical_mapping
 
-rural = df_from_orgfile('./categorical_mapping.org',name='rural',encoding='ISO-8859-1')
-rurald = rural.set_index('Code').to_dict('dict')['Label']
-region = df_from_orgfile('../../_/categorical_mapping.org',name='region',encoding='ISO-8859-1')
-regiond = region.set_index('Code').to_dict('dict')['Label']
+regiond = get_categorical_mapping(tablename='region')
+rurald = get_categorical_mapping(tablename='rural')
 
-idxvars = dict(j=(['clust','nh'],lambda x: format_id(x.clust)+format_id(x.nh)),
-               t=('nh', lambda x: "1991-92"),
-               m=('region',lambda x: regiond[f"{x:3.0f}".strip()]),
+idxvars = dict(h=(['clust','nh'],lambda x: format_id(x.clust)+format_id(x.nh)),
+               w=('nh', lambda x: "1991-92"),
                )
 
 myvars = dict(v=('clust',format_id),
+              Region=('region',lambda x: regiond[f"{x:3.0f}".strip()]),
               Rural=('loc2',rurald))
 
 of = df_data_grabber('../Data/POV_GH.DTA',idxvars,**myvars)
 
-of = of.reset_index('m').groupby('v').head(1).reset_index().set_index(['j','t','v'])
-of = of.droplevel('j')
+# Aggregate to cluster level
+of = of.groupby('v').head(1).reset_index().set_index(['h','w','v'])
+of = of.droplevel('h')
 
 if __name__=='__main__':
     to_parquet(of,'other_features.parquet')
--- GhanaLSS/1991-92/_/household_roster.py
+++ GhanaLSS/1991-92/_/household_roster.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 import numpy as np
-import pandas as pd
 import sys
 sys.path.append('../../../_/')
-from local_tools import df_data_grabber, to_parquet, df_from_orgfile, format_id
+from local_tools import df_data_grabber, to_parquet, df_from_orgfile, format_id, get_categorical_mapping
 from collections import defaultdict
+import warnings
 
-relationship_mapping = df_from_orgfile('../../_/categorical_mapping.org',name='relationship',encoding='ISO-8859-1')
-relationship_mapping = relationship_mapping.set_index('Code').to_dict('dict')['Label']
+relationship_mapping = get_categorical_mapping(tablename='relationship')
 
-region_mapping = df_from_orgfile('../../_/categorical_mapping.org',name='region',encoding='ISO-8859-1')
-region_mapping = region_mapping.set_index('Code').to_dict('dict')['Label']
+region_mapping = get_categorical_mapping(tablename='region')
 region_mapping = defaultdict(lambda:None,region_mapping)
 
-idxvars = dict(j=(['clust','nh'],lambda x: format_id(x.clust)+format_id(x.nh)),
-               t=('nh', lambda x: "1991-92"),
+idxvars = dict(h=(['clust','nh'],lambda x: format_id(x.clust)+format_id(x.nh)),
+               w=('nh', lambda x: "1991-92"),
                v=('clust',format_id),
                indiv=('pid',lambda x: format_id(x)))
 
@@ -29,7 +27,9 @@ df = df_data_grabber('../Data/S1.DTA',idxvars,**myvars)
 # Oddity with large number for missing code
 na = df.select_dtypes(exclude='object').max().max()
 
-#df = df.replace(na,np.nan)
+if na==99 or na>1e99:  # Missing values?
+    warnings.warn(f"Large number used for missing?  Replacing {na} with NaN.")
+    df = df.replace(na,np.nan)
 
 if __name__=='__main__':
     to_parquet(df,'household_roster.parquet')
