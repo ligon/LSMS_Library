d3b68bc5e391c67e1e6b683659de08d69731cd02
Ethan Ligon <ligon@berkeley.edu>
2025-01-03 10:08:20 -0800

    Update household roster to (h,w,v) indices.

--- GhanaLSS/2016-17/_/household_roster.py
+++ GhanaLSS/2016-17/_/household_roster.py
@@ -3,20 +3,19 @@ import numpy as np
 import pandas as pd
 import sys
 sys.path.append('../../../_/')
-from local_tools import df_data_grabber, to_parquet, df_from_orgfile, format_id
+from local_tools import df_data_grabber, to_parquet, df_from_orgfile, format_id, get_categorical_mapping
 from collections import defaultdict
 import warnings
 
-relationship_mapping = df_from_orgfile('../../_/categorical_mapping.org',name='relationship',encoding='ISO-8859-1')
-relationship_mapping = relationship_mapping.set_index('Code').to_dict('dict')['Label']
+relationship_mapping = get_categorical_mapping(tablename='relationship')
 
-region_mapping = df_from_orgfile('../../_/categorical_mapping.org',name='region',encoding='ISO-8859-1')
-region_mapping = region_mapping.set_index('Code').to_dict('dict')['Label']
+region_mapping = get_categorical_mapping(tablename='region')
 region_mapping = defaultdict(lambda:None,region_mapping)
 
-idxvars = dict(j=('hid',lambda x: format_id(x)),
-               t=('hid', lambda x: "2016-17"),
-               indiv=('pid',lambda x: format_id(x))
+idxvars = dict(h=('hid',format_id),
+               w=('hid', lambda x: "2016-17"),
+               v = ('clust',format_id),
+               indiv=('pid',format_id)
                )
 
 myvars = dict(Sex = ('s1q2', lambda s: s.upper()[0]),
@@ -25,6 +24,9 @@ myvars = dict(Sex = ('s1q2', lambda s: s.upper()[0]),
               Relation = ('s1q3', lambda x: x.title()))
 
 df = df_data_grabber('../Data/g7sec1.dta',idxvars,**myvars,convert_categoricals=True)
+
+#assert len(set(df.Birthplace.value_counts().index.tolist()).difference(region_mapping.values()))==0
+#assert len(set(df.Relation.value_counts().index.tolist()).difference(relationship_mapping.values()))==0
 
 # Oddity with large number for missing code
 na = df.select_dtypes(exclude='object').max().max()
