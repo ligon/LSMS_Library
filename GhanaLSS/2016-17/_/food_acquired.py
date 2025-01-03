3e35240588cde18f813d5b3b64fe2a691a777e8b
Ethan Ligon <ligon@berkeley.edu>
2025-01-03 07:37:51 -0800

    Revamp food_acquired to loop over visits.

--- GhanaLSS/2016-17/_/food_acquired.py
+++ GhanaLSS/2016-17/_/food_acquired.py
@@ -1,90 +1,76 @@
 #!/usr/bin/env python
 import sys
-from lsms.tools import from_dta
 import numpy as np
-import dvc.api
 import pandas as pd
 sys.path.append('../../../_/')
-from local_tools import df_from_orgfile
+from local_tools import df_from_orgfile, get_categorical_mapping, format_id, df_data_grabber, _to_numeric, to_parquet
+import warnings
+from collections import defaultdict
 
-t = '2016-17'
+w = '2016-17'
+Visits = range(1,7)
 
-selector_pur = {'hhid': 'j',
-                'freqcd': 'i'}
+# categorical mapping
+labelsd = get_categorical_mapping(tablename='harmonize_food',idxvars={'Code':('Code_9b',format_id)},**{'Label':'Preferred Label'})
+unitsd = defaultdict(lambda:np.nan,get_categorical_mapping(tablename='units'))
 
-#categorical mapping
-labels = df_from_orgfile('./categorical_mapping.org',name='harmonize_food',encoding='ISO-8859-1')
-labelsd = {}
-for column in ['Code_9b', 'Code_8h']:
-    labelsd[column] = labels[['Preferred Label', column]].set_index(column).to_dict('dict')
-#units = df_from_orgfile('./categorical_mapping.org',name='s8hq9',encoding='ISO-8859-1')
-#unitsd = units.set_index('Code').to_dict('dict')
+# food expenditure
+idxvars = dict(h=(['clust','nh'],lambda x: format_id(x.clust)+format_id(x.nh)),
+               w=('nh',lambda x: w),
+               v=('clust',format_id),
+               j=('freqcd',lambda x: labelsd[format_id(x)]))
 
-#food expenditure
-with dvc.api.open('../Data/g7sec9b_small.dta',mode='rb') as dta:
-    df = from_dta(dta, convert_categoricals=False)
-    #harmonize food labels
-    df['freqcd'] = df['freqcd'].replace(labelsd['Code_9b']['Preferred Label'])
+visit = {}
+# Iterate over visits
+for i in Visits:
+    idxvars['u'] = (f"s9bq{i}c",lambda x: unitsd[_to_numeric(x)])
 
-df['hhid'] = (df.apply(lambda x:f"{int(x['clust']):d}/{int(x['nh']):02d}",axis=1))
-#df['clust'].astype(int).astype("string")+'/'+df['nh'].astype(int).astype("string")
+    myvars = {f"Expenditure":(f"s9bq{i}a",_to_numeric),
+              f"Quantity":(f"s9bq{i}b",_to_numeric)}
 
+    visit[i] = df_data_grabber('../Data/g7sec9b_small.dta',idxvars,convert_categoricals=False,**myvars)
+    visit[i] = visit[i].replace(0,np.nan).dropna(how='all')
+    visit[i] = visit[i].groupby(list(idxvars.keys())).sum()
 
-#create purchased column labels for each visit -- from the 2nd to 11th visit
-selector_pur.update({f's9bq{i}a':f'purchased_value_v{i}' for i in range(1,7)})
-selector_pur.update({f's9bq{i}b':f'purchased_quantity_v{i}' for i in range(1,7)})
-selector_pur.update({f's9bq{i}c':f'purchased_unit_v{i}' for i in range(1,7)})
+x = pd.concat(visit,names=['visit']+visit[1].index.names)
 
-x = df.rename(columns=selector_pur)[[*selector_pur.values()]]
-x = x.replace({r'':np.nan, 0 : np.nan})
-x = x.groupby(['j','i']).sum() # Deal with some cases with multiple records for purchases
-x = pd.wide_to_long(x.reset_index(),['purchased_value','purchased_quantity','purchased_unit'],['j','i'],'visit',sep='_v')
-x = x.replace(0,np.nan).dropna(how='all')
+####################
+# Home produced
+####################
 
-# Only select food expenditures,since section9b also recorded non-food expenditures.
-x = x.loc[x.index.isin(labelsd['Code_9b']['Preferred Label'].values(),level='i')]
+Visits = range(3,8)
+labelsd = get_categorical_mapping(tablename='harmonize_food',idxvars={'Code':('Code_8h',format_id)},**{'Label':'Preferred Label'})
 
-# Add null unit index
-x = x.rename(columns={'purchased_unit':'u'})
-x = x.reset_index().set_index(['j','i','u','visit'])
+# food quantities
+idxvars = dict(h=(['clust','nh'],lambda x: format_id(x.clust)+format_id(x.nh)),
+               w=('nh',lambda x: w),
+               v=('clust',format_id),
+               j=('foodcd',lambda x: labelsd[format_id(x)]))
 
-# Home produced amounts
-with dvc.api.open('../Data/g7sec8h.dta',mode='rb') as dta:
-    prod = from_dta(dta, convert_categoricals=False)
-    #harmonize food labels and map unit labels:
-    #prod['foodcd'] = prod['foodcd'].replace(labelsd['Code_8h']['Preferred Label'])
-    #prod['s8hq13'] = prod['s8hq13'].replace(unitsd['Label'])
+visit = {}
+# Iterate over visits
+for i in Visits:
+    myvars = {'Price':(f's8hq{i}p',_to_numeric),
+              "Produced":(f"s8hq{i}q",_to_numeric)}
+    idxvars['u'] = (f's8hq{i}u',lambda x: unitsd[x])
 
-prod = prod[prod['s8hq1'] == 1] #select only if hh consumed any own produced food in the past 12 months
-#create produced column labels for each visit -- 3-day recall starting from the 2nd to 7th visit
+    visit[i] = df_data_grabber('../Data/g7sec8h.dta',idxvars,convert_categoricals=False,**myvars)
+    visit[i] = visit[i].replace(0,np.nan).dropna(how='all')
 
-prod['hhid'] = (prod.apply(lambda x:f"{int(x['clust']):d}/{int(x['nh']):02d}",axis=1))
+prod = pd.concat(visit,names=['visit']+visit[3].index.names)
 
-selector_pro = {'hhid': 'j',
-                'foodcd': 'i'}
+# Oddity with large number for missing code
+na = prod.select_dtypes(exclude='object').max().max()
 
-selector_pro.update({f's8hq{i}q':f'produced_quantity_v{i}' for i in range(3,9)})
-selector_pro.update({f's8hq{i}u':f'produced_unit_v{i}' for i in range(3,9)})
-selector_pro.update({f's8hq{i}p':f'produced_price_v{i}' for i in range(3,9)})
+if na>1e99:  # Missing values?
+    warnings.warn(f"Large number used for missing?  Replacing {na} with NaN.")
+    prod = prod.replace(na,np.nan)
 
-y = prod.rename(columns=selector_pro)[[*selector_pro.values()]]
-#unit code 1.7498005798264095e+100 has no categorical mapping
-y.index = y.index.map(str)
+prod = prod.groupby(prod.index.names).sum()
 
-#unstack by visits
-y = y.replace({r'':np.nan, 0 : np.nan})
-y = y.dropna(how='all')
-y = pd.wide_to_long(y.reset_index(),['produced_quantity','produced_unit','produced_price'],['j','i'],'visit',sep='_v')
-y = y.rename(labelsd['Code_8h']['Preferred Label'],level='i')
+fa = x.join(prod,how='outer')
 
-y = y.join(x,how='outer')
+fa = fa.dropna(how='all')
 
-y['t'] = t
-y = y.reset_index().set_index(['j','t','i','u','visit'])
-
-fa = y.groupby(['j','t','i','u']).sum()
-fa = fa.replace(0,np.nan).dropna(how='all')
-
-# Deal with non-string values in units
-fa.index = fa.index.set_levels(fa.index.levels[3].astype(str),level='u')
-fa.to_parquet('food_acquired.parquet')
+if __name__=='__main__':
+    to_parquet(fa,'food_acquired.parquet')
