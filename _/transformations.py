7fed74807f0f9f443015c4e77a9d895d47bce6d3
Ethan Ligon <ligon@berkeley.edu>
2025-02-18 16:47:41 -0800

    Add top-level transformations library to repo.

---
+++ _/transformations.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+A collection of mappings to transform dataframes.
+"""
+import pandas as pd
+
+def age_intervals(age,age_cuts=(0,4,9,14,19,31,51)):
+    """
+    Take as input a Series (e.g., a row from a dataframe), and use variables =Age= and =Sex=
+    to create a set of coarser categories.
+    """
+    age_cuts = [-np.inf]+list(age_cuts)+[np.inf]
+    return pd.cut(age,age_cuts,duplicates='drop')
+
+def dummies(df,cols,suffix=False):
+    """From a dataframe df, construct an array of indicator (dummy) variables,
+    with a column for every unique row df[cols]. Note that the list cols can
+    include names of levels of multiindices.
+
+    The optional argument =suffix=, if provided as a string, will append suffix
+    to column names of dummy variables. If suffix=True, then the string '_d'
+    will be appended.
+    """
+    idxcols = list(set(df.index.names).intersection(cols))
+    colcols = list(set(cols).difference(idxcols))
+
+    v = concat([use_indices(df,idxcols),df[colcols]],axis=1)
+
+    usecols = []
+    for s in idxcols+colcols:
+        usecols.append(v[s].squeeze())
+
+    tuples = pd.Series(list(zip(*usecols)),index=v.index)
+
+    v = get_dummies(tuples).astype(int)
+
+    if suffix==True:
+        suffix = '_d'
+
+    if suffix!=False and len(suffix)>0:
+        columns = [tuple([str(c)+suffix for c in t]) for t in v.columns]
+    else:
+        columns = v.columns
+
+    v.columns = MultiIndex.from_tuples(columns,names=idxcols+colcols)
+
+    return v
