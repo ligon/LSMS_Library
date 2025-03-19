f380ad07aeb74f2e80bd40c26f5d79c2d86b4a82
Elaine Zhang <ehzhang@berkeley.edu>
2025-03-18 17:21:01 -0700

    ghanalss years

---
+++ GhanaLSS/1998-99/_/1998-99.py
@@ -0,0 +1,59 @@
+# Formatting  Functions for Ghana 1998-99
+import pandas as pd
+import numpy as np
+import lsms_library.local_tools as tools
+from collections import defaultdict
+
+region_dict = tools.get_categorical_mapping(tablename = 'region', dirs=['../../_/', '../_/', './_/', '.'])
+rural_dict = tools.get_categorical_mapping(tablename = 'rural', dirs=['1998-99/_/', '../../1998-99/_/', '../1998-99/_/', '.', './_/'])
+
+def i(value):
+    '''
+    Formatting household id
+    '''
+    return tools.format_id(value.iloc[0])+tools.format_id(value.iloc[1],zeropadding=2)
+
+def Sex(value):
+    '''
+    Formatting sex veriable
+    '''
+    return (lambda s: 'MF'[int(s)-1])(value)
+
+def Age(value):
+    '''
+    Formatting age variable
+    '''
+    return int(value)
+
+def Birthplace(value):
+    '''
+    Formatting birthplace variable
+    '''
+    if value > 1e99:
+        print('extremely large value warning?', value)
+        return np.nan
+    return (lambda x: region_dict[f"{x:3.0f}".strip()])(value)
+
+def Relation(value):
+    '''
+    Formatting relationship variable
+    '''
+    relationship_dict = tools.get_categorical_mapping(tablename = 'relationship', dirs=['../../_/', '../_/', './_/', '.'])
+    return relationship_dict.get(value, np.nan)
+
+def Region(value):
+    '''
+    Formatting region variable
+    '''
+
+    return region_dict.get(str(int(value)), np.nan)
+    
+
+def Rural(value):
+    '''
+    Formatting rural variable
+    '''
+
+    return rural_dict.get(value)
+
+Visits = range(1,7)
