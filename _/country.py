cada1a6a72559d34cd2e766400266bde68223b83
cefayefang <ce_fang@berkeley.edu>
2025-05-08 19:10:09 -0700

    debug panel_id feature in Coutry.py

--- _/country.py
+++ _/country.py
@@ -412,7 +412,7 @@ class Country:
         """
         if method_name not in self.data_scheme and method_name not in ['other_features', 'food_prices_quantities_and_expenditures', 'updated_ids']:
             warnings.warn(f"Data scheme does not contain {method_name} for {self.name}")
-            return None
+            return pd.DataFrame()
 
         if waves is None:
             waves = self.waves
@@ -478,12 +478,12 @@ class Country:
         Compute and cache both panel_ids and updated_ids.
         """
         panel_ids_dic = self._aggregate_wave_data(None, 'panel_ids')
-        if panel_ids_dic:
-            if isinstance(panel_ids_dic, dict):
-                updated_ids_dic = self._aggregate_wave_data(None, 'updated_ids')
-            elif isinstance(panel_ids_dic, pd.DataFrame):
-                panel_ids_dic, updated_ids_dic = panel_ids(panel_ids_dic)
-                # panel_ids_dic = panel_ids_dic.data
+
+        if isinstance(panel_ids_dic, dict):
+            updated_ids_dic = self._aggregate_wave_data(None, 'updated_ids')
+        elif isinstance(panel_ids_dic, pd.DataFrame) and not panel_ids_dic.empty:
+            panel_ids_dic, updated_ids_dic = panel_ids(panel_ids_dic)
+            # panel_ids_dic = panel_ids_dic.data
             self._panel_ids_cache = panel_ids_dic
             self._updated_ids_cache = updated_ids_dic
         else:
